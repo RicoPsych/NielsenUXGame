07bee8aef3a7e255f8805883c7ef94a057852071
rico <h.nacmer@gmail.com>
2024-05-09 19:55:00 +0200

    fixed - mouse cursor

--- Assets/Player/CursorChanger.cs
+++ Assets/Player/CursorChanger.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CursorChanger : MonoBehaviour
 {
     public CursorType cursorType;
     private GameObject player;
+    private GameObject browser;
     public SpriteRenderer highlight;
     
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        browser = GameObject.Find("Browser (Mouse)");
         //transform.GetChild(0).
         highlight = gameObject.transform.GetChild(0).GetComponentInChildren<SpriteRenderer>();
     }
@@ -26,5 +29,16 @@ public class CursorChanger : MonoBehaviour
         highlight.color = Color.green;
         player.GetComponent<Player>().cursor = cursorType;
         Debug.Log($"Changed CursorType {cursorType}");
+
+        if(cursorType == CursorType.Mouse)
+        {
+            browser.GetComponent<ZenFulcrum.EmbeddedBrowser.PointerUIMesh>().enableMouseInput = true;
+            browser.GetComponent<ZenFulcrum.EmbeddedBrowser.PointerUIMesh>().enableTouchInput = true;
+        }
+        else 
+        {
+            browser.GetComponent<ZenFulcrum.EmbeddedBrowser.PointerUIMesh>().enableMouseInput = false;
+            browser.GetComponent<ZenFulcrum.EmbeddedBrowser.PointerUIMesh>().enableTouchInput = false;
+        }
     }
 }
