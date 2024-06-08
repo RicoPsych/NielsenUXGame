d364c941764bc90021ecd4ecae7b7526975ce1fb
rico <h.nacmer@gmail.com>
2024-06-08 19:17:17 +0200

    Background

--- Assets/Insect/InsectSpawnerScript.cs
+++ Assets/Insect/InsectSpawnerScript.cs
@@ -1,18 +1,26 @@
+//using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class InsectSpawnerScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float delay;
+    public float minDelay;
+    public float maxDelay;
+
+    private float delay;
     private float timer;
 
-    public int batchSize;
+
+    public float batchDelayRatio;
+    private int batchSize;
     public GameObject insect;
     void Start()
     {
-        
+        delay = UnityEngine.Random.Range(minDelay, maxDelay);
+        batchSize = (int)math.ceil(delay/batchDelayRatio) ;
     }
 
     // Update is called once per frame
@@ -25,9 +33,13 @@ public class InsectSpawnerScript : MonoBehaviour
             for (int i = 0;i<batchSize;i++)
             {
                 insect.transform.position = this.transform.position;
-                insect.transform.Rotate(0,0, Random.Range(0,360));
+                insect.transform.Rotate(0,0, UnityEngine.Random.Range(0,360));
+                
                 Instantiate(insect);
             }
+            delay = UnityEngine.Random.Range(minDelay, maxDelay);
+            batchSize = (int)math.ceil(delay/5);
+            Debug.Log($"Next bug batch of size {batchSize} in {delay} s");
         }
 
     }
